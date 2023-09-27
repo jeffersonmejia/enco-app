7e7b3ec5f41a901cbb2b1585f7fafd63826abaa8
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-09-27 12:11:47 -0500

    add new package form

--- scripts/client-dashboard.js
+++ scripts/client-dashboard.js
@@ -1,12 +1,23 @@
 const d = document,
-	$account = d.querySelector('.account-navbar-items')
+	$account = d.querySelector('.account-navbar-items'),
+	$packageForm = d.querySelector('.package-form-modal')
 
 function toggleAccountItems() {
 	$account.classList.toggle('hidden')
 }
 
+function togglePackageForm() {
+	$packageForm.classList.toggle('hidden')
+}
+
 d.addEventListener('click', (e) => {
 	if (e.target.matches('.account-navbar')) {
 		toggleAccountItems()
+	}
+	if (e.target.matches('.new-package-button')) {
+		togglePackageForm()
+	}
+	if (e.target.matches('.cancel-new-package')) {
+		togglePackageForm()
 	}
 })
