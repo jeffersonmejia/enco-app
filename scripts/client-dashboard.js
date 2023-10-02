e7d7807bd118fc45e05e3adf9361644a29871ab8
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-10-02 16:24:50 -0500

    date and number bill

--- scripts/client-dashboard.js
+++ scripts/client-dashboard.js
@@ -5,7 +5,9 @@ const d = document,
 	$packageDescription = d.querySelector('.package-description'),
 	$accountModal = d.querySelector('.account-modal'),
 	$accountEdit = d.querySelector('.modal-account-edit'),
-	$accountModalItems = d.querySelector('.account-modal-items')
+	$accountModalItems = d.querySelector('.account-modal-items'),
+	$billNumberItems = d.querySelectorAll('[data-bill-number]'),
+	$billDateItems = d.querySelectorAll('[data-bill-date]')
 
 let currentSettingSection = ''
 
@@ -41,6 +43,66 @@ function toggleAccountSection(section) {
 	$accountEdit.classList.toggle('hidden')
 	sectionId?.classList.toggle('hidden')
 }
+
+function loadBillsNumber() {
+	if (window.innerWidth >= 600) {
+		$billNumberItems.forEach((bill) => {
+			const number = bill.dataset.billNumber
+			bill.innerHTML = number
+		})
+	} else {
+		$billNumberItems.forEach((bill) => {
+			const number = bill.dataset.billNumber,
+				sliced = number.slice(-4)
+			bill.innerHTML = `...${sliced}`
+		})
+	}
+}
+
+function loadBillDate() {
+	const currentDate = new Date(),
+		currentMonth = currentDate.getMonth() + 1,
+		currentDay = currentDate.getDay() + 1
+
+	$billDateItems.forEach((bill) => {
+		let [year, month, day] = bill.dataset.billDate.split('-')
+		year = parseInt(year)
+		month = parseInt(month)
+		day = parseInt(day)
+
+		const monthDif = currentMonth - month,
+			dayDif = currentDay - day
+		if (dayDif !== 0) {
+			if (dayDif === 1) {
+				bill.innerHTML = `Hace ${dayDif} día`
+			}
+			if (dayDif > 1) {
+				bill.innerHTML = `Hace ${dayDif} días`
+			}
+		}
+		if (monthDif !== 0) {
+			if (monthDif === 1) {
+				bill.innerHTML = `Hace ${monthDif} mes`
+			}
+			if (monthDif > 1) {
+				bill.innerHTML = `Hace ${monthDif} meses`
+			}
+		}
+		if (month < 9) {
+			month = `0${month}`
+		}
+		if (day < 9) {
+			day = `0${day}`
+		}
+		bill.style.cursor = 'pointer'
+		bill.title = `${year}-${month}-${day}`
+	})
+}
+
+d.addEventListener('DOMContentLoaded', (e) => {
+	loadBillDate()
+	loadBillsNumber()
+})
 
 d.addEventListener('click', (e) => {
 	if (e.target.matches('.account-navbar')) {
