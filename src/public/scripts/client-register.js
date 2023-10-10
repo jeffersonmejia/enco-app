14bb9ca2c7f6be32897236a97e77c28f911b5649
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-10-09 19:57:17 -0500

    fix forms

--- src/public/scripts/client-register.js
+++ src/public/scripts/client-register.js
@@ -4,13 +4,16 @@ const d = document,
 	$formGeneral = d.getElementById('form-general'),
 	$formAddress = d.getElementById('form-address'),
 	$formSecurity = d.getElementById('form-security'),
-	$formPayment = d.getElementById('form-payment')
+	$formPayment = d.getElementById('form-payment'),
+	$backButton = d.getElementById('back-payment')
 
 const API = 'localhost:3000/clientes-registrar'
 let user = {}
 
 function toggleForm(prevForm, nextForm) {
-	const prevInputs = prevForm.querySelectorAll('input[type="text"]'),
+	const prevInputs = prevForm.querySelectorAll(
+			'input[type="text"],input[type="password"], select'
+		),
 		prevData = getInputsValue(prevInputs)
 	user = { ...user, ...prevData }
 	if (nextForm) {
@@ -19,9 +22,21 @@ function toggleForm(prevForm, nextForm) {
 	nextForm?.classList.toggle('hidden')
 }
 
+function disableInputs() {
+	const formInputs = d.querySelectorAll('input, select'),
+		inputs = Array.from(formInputs)
+	inputs.forEach((input) => {
+		input.disabled = !input.disabled
+	})
+}
+
 function getInputsValue(inputs) {
 	const copy = Array.from(inputs),
 		arrayData = copy.map((input) => {
+			if (input.type === 'select-one') {
+				const option = input.querySelector(`option[value="${input.value}"]`)
+				return [input.name, option.textContent]
+			}
 			return [input.name, input.value]
 		}),
 		data = Object.fromEntries(arrayData)
@@ -47,6 +62,11 @@ d.addEventListener('click', async (e) => {
 	}
 	if (e.target.matches('#submit-payment')) {
 		toggleForm($formPayment, null)
+		e.target.value = 'Registrando...'
+		e.target.style.cursor = 'not-allowed'
+		e.target.style.disabled = true
+		$backButton.style.visibility = 'hidden'
+		disableInputs()
 		//await registerUser(user)
 	}
 	if (e.target.matches('#back-address')) {
